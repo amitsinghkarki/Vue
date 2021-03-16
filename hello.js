5aaf79e82d010c88c63cad4e3ef30d37e21b3112
amitsinghkarki <batheo54@gmail.com>
2021-03-16 17:25:55 +0530

    gh repo clone amitsinghkarki/Vue

---
+++ hello.js
@@ -0,0 +1,68 @@
+var vm = new Vue({
+    el: '#example',
+    data: {
+        msg: 'the world',
+        seen:false
+    },
+        created: function () {
+            // `this` points to the vm instance
+            console.log('seen is: ' +this.seen)
+          },
+        methods: {
+            reverseMessage: function () {
+              this.msg = this.msg.split('').reverse().join(''),
+            this.seen=!this.seen
+    }
+        },
+     
+
+})
+vm.$watch('seen', function (newValue, oldValue) {
+    // This callback will be called when `vm.a` changes
+    console.log('seen is: ' +this.seen)
+  })
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+      message: 'You loaded this page on ' + new Date().toLocaleString(),
+    }
+  })
+
+
+  Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+  })
+  
+  var app7 = new Vue({
+    el: '#app-7',
+    data: {
+      groceryList: [
+        { id: 0, text: 'Vegetables' },
+        { id: 1, text: 'Cheese' },
+        { id: 2, text: 'Whatever else humans are supposed to eat' }
+      ]
+    }
+  })
+
+  var vm = new Vue({
+    el: '#demo',
+    data: {
+      firstName: 'Foo',
+      lastName: 'Bar'
+    },
+    computed: {
+        fullName: {
+          // getter
+          get: function () {
+            return this.firstName + ' ' + this.lastName
+          },
+          // setter
+          set: function (newValue) {
+            var names = newValue.split(' ')
+            this.firstName = names[0]
+            this.lastName = names[names.length - 1]
+          }
+        }
+      }
+  })
