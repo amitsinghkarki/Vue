37c48d5eb78a518c4581b24ce260608ccac0f8fa
amitsinghkarki <41285949+amitsinghkarki@users.noreply.github.com>
2021-03-17 20:00:55 +0530

    Add files via upload

--- hello.js
+++ hello.js
@@ -12,6 +12,13 @@ var vm = new Vue({
             reverseMessage: function () {
               this.msg = this.msg.split('').reverse().join(''),
             this.seen=!this.seen
+            
+            
+            //Filter example changes
+            /*  
+            example1.items = example1.items.filter(function (item) {
+              return item.message.match(/r/)
+            })*/
     }
         },
      
@@ -42,5 +49,69 @@ var app2 = new Vue({
         { id: 1, text: 'Cheese' },
         { id: 2, text: 'Whatever else humans are supposed to eat' }
       ]
+    }
+  })
+
+  var vm = new Vue({
+    el: '#demo',
+    data: {
+      firstName: 'Foo',
+      lastName: 'Bar'
+    },
+    computed: {
+        fullName: {
+          // getter
+          get: function () {
+            return this.firstName + ' ' + this.lastName
+          },
+          // setter
+          set: function (newValue) {
+            var names = newValue.split(' ')
+            this.firstName = names[0]
+            this.lastName = names[names.length - 1]
+          }
+        }
+      }
+  })
+
+  var example1 = new Vue({
+    el: '#example-1',
+    data: {
+      items: [
+        { message: 'Foo' },
+        { message: 'Bar' },
+        { message: 'Var' },
+        { message: 'Test' }
+
+      ]
+    }
+  })
+
+
+  /// To add or change values of array using command prompt
+  ////Vue.set(example2.items, 3, ({message:'Hello'}))
+
+  var example2 = new Vue({
+    el: '#example-2',
+    data: {
+      parentMessage:'Parentmesage',
+      items: [
+        { message: 'Foo' },
+        { message: 'Bar' },
+        { message: 'Var' },
+        { message: 'Test' }
+
+      ]
+    }
+  })
+
+  var example3= new Vue({
+    el: '#v-for-object',
+    data: {
+      object: {
+        title: 'How to do lists in Vue',
+        author: 'Jane Doe',
+        publishedAt: '2016-04-10'
+      }
     }
   })
