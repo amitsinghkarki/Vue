9d05d229b57b3e0a28370e84d732a949a807752b
amitsinghkarki <amitsinghkarki@users.noreply.github.com>
2021-03-21 18:51:18 +0530

    practice at home

--- index.js
+++ index.js
@@ -8,3 +8,23 @@ var example1 = new Vue({
   }
 })
  
+var example2 = new Vue({
+  el: '#example-2',
+  data: {
+    name: 'Vue.js'
+  },
+  // define methods under the `methods` object
+  methods: {
+    greet: function (event) {
+      // `this` inside methods points to the Vue instance
+      alert('Hello ' + this.name + '!')
+      // `event` is the native DOM event
+      if (event) {
+        alert(event.target.tagName)
+      }
+    }
+  }
+})
+
+// you can invoke methods in JavaScript too
+example2.greet() // => 'Hello Vue.js!'
